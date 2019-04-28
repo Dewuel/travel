750117d1bdbb50c992aaabd19e8ae6f922b20246
Dewuel <Dewuel@users.noreply.github.com>
2019-04-29 03:48:55 +0800

    project init

---
+++ postcss.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    plugins: {
+        'autoprefixer': {browser: 'last 5 version'}
+    }
+}
--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import fastClick from 'fastclick'
 import router from './router'
 import store from './store'
+import 'normalize.css'
+import '@/assets/border.css'
 
 Vue.config.productionTip = false
+
+fastClick.attach(document.body)
 
 new Vue({
   router,
